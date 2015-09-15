3884d86244f7c766c6b9ef7a788cbbda6153787e
Adriano Valente <dri9595@gmail.com>
2015-09-15 10:36:13 -0300

    Working for one single byte

--- exp2/TYG14A02E03.asm
+++ exp2/TYG14A02E03.asm
@@ -13,11 +13,20 @@ TO_PASTE     K /0010
              K /FFFF
              K /FFFF
 ZERO_CONST   K /0000
+ONE_CONST    K /0001
 ERROR_CONST  K /FFFF
+LOAD        LD /0000
+STORE       MM /0000
+BYTE         $ /0001
 
 
 INICIO      SC VERIFY      ; Verifica se pode haver erro de memória
             JZ START_COPY  ; Somente começa a cópia se há espaço disponível
+FIM         HM FIM
+
+START_COPY  SC COPY
+
+
 
 ; ##########################
 ; ##    VERIFY ROUTINE    ##
@@ -36,7 +45,43 @@ VERIFY       $ /0001
 ; não há espaço disponível para realizar a operação
 
             JN ERROR        ; Neste caso temos que retornar o erro
-            LD ZERO         ; Procedimento padrão para retornar sucesso
+            LD ZERO_CONST   ; Procedimento padrão para retornar sucesso
 VERIFY_OK   RS VERIFY
 ERROR       LD ERROR_CONST
             JP VERIFY_OK
+
+
+; ############################
+; ##   START_COPY ROUTINE   ##
+; ############################
+
+COPY         $ /0001
+            JP COPY_BYTE
+COPY_BYTE   LD SIZE
+            JZ FINISH       ; Se SIZE = 0 significa que a cópia acabou
+             - ONE_CONST    ; Atualizando o tamanho da palavr a ser copiada
+            MM SIZE
+
+            LD TO_COPY
+             + LOAD
+            MM LOAD_LINE
+LOAD_LINE    K /0000
+            MM BYTE
+            LD TO_PASTE
+             + STORE
+            MM STORE_LINE
+            LD BYTE
+STORE_LINE   K /0000
+
+            LD TO_COPY
+             + ONE_CONST    ; Atualizando o ponteiro TO_COPY
+            MM TO_COPY
+            LD TO_PASTE
+             + ONE_CONST    ; Atualizando o ponteiro TO_PASTE
+            MM TO_PASTE
+            ;JP COPY_BYTE
+            
+
+FINISH      RS COPY
+
+# MAIN
