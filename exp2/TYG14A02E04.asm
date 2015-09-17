00d4602fb435c1671ddfda91e5ad45209588a1d5
Adriano Valente <dri9595@gmail.com>
2015-09-17 13:48:42 -0300

    Refactoring some constants

--- exp2/TYG14A02E04.asm
+++ exp2/TYG14A02E04.asm
@@ -24,59 +24,61 @@ SEGUNDA     K /0014
 
 ; #### Variaveis auxiliares ####
 
+A           $ /0001
+B           $ /0001
+A_1         $ /0001
+A_2         $ /0001
+B_1         $ /0001
+B_2         $ /0001
+SAIDA1      $ /0001
+SAIDA2      $ /0001
+PACKED      $ /0001
+CONST_100   K /0100
+CONST_FF    K /FF00
+CONST_01    K /0001
+CONST_02    k /0002
+AUX         $ /0001
+X           $ /0001
 
-A $ /0001
-B $ /0001
-A_1 $ /0001
-A_2 $ /0001
-B_1 $ /0001
-B_2 $ /0001
-SAIDA1 $ /0001
-SAIDA2 $ /0001
-PACKED $ /0001
-CONST_100 K /0100
-CONST_FF K /FF00
-CONST_01 K /0001
-AUX $ /0001
-X $ /0001
-
-LOAD LD /0000
-STORE MM /0000
-
-; #### Constantes uteis ####
-CONST_2 K /0002
-CONST_1 K /0001
-CONST_MAX_UM_BYTE K /00FF
+LOAD       LD /0000
+STORE      MM /0000
 
 ;  ######################
 ;  ##      INICIO      ##
 ;  ######################
 
+; Automodificação para carregar o primeiro endereço
 COMECAR LD PRIMEIRA
 + LOAD
 MM LOAD_PRIMEIRA
 LOAD_PRIMEIRA $ /0001
 
+; Chamando UNPACK e guardando os resultados em variáveis auxiliares
 MM PACKED
 SC UNPACK
-
 LD SAIDA1
 MM A_1
 LD SAIDA2
 MM A_2
 
+; Automodificação para carregar o segundo endereço
 LD SEGUNDA
 + LOAD
 MM LOAD_SEGUNDA
 LOAD_SEGUNDA $ /0001
 
+; Chamando UNPACK again e guardando os resultados em variáveis auxiliares
 MM PACKED
 SC UNPACK
-
 LD SAIDA1
 MM B_1
 LD SAIDA2
 MM B_2
+
+
+; ###################################
+; ##         COMPARACOES           ##
+; ###################################
 
 LD A_1
 JZ FIM
@@ -84,7 +86,7 @@ LD B_1
 JZ FIM
 - A_1
 JZ PRIMEIRO_IGUAL
-JP FIM
+JP FIM ; Finaliza a execução se as strings não forem iguais
 
 
 COMPARA_SEGUNDO LD A_2
@@ -93,16 +95,18 @@ LD B_2
 JZ FIM
 - A_2
 JZ SEGUNDO_IGUAL
-JP FIM
+JP FIM ; Finaliza a execução se as strings não forem iguais
 
-; ATUALIZA CONTADOR
+; ###################################
+; ##    Atualizando ponteiros      ##
+; ###################################
 
 ATUALIZA_CONTADOR LD PRIMEIRA
-+ CONST_2
++ CONST_02
 MM PRIMEIRA
 
 LD SEGUNDA
-+ CONST_2
++ CONST_02
 MM SEGUNDA
 
 JP COMECAR
@@ -110,16 +114,19 @@ JP COMECAR
 FIM HM FIM                  ; Fim do programa
 
 PRIMEIRO_IGUAL LD SAIDA
-+ CONST_1
++ CONST_01
 MM SAIDA
 JP COMPARA_SEGUNDO
 
 SEGUNDO_IGUAL LD SAIDA
-+ CONST_1
++ CONST_01
 MM SAIDA
 JP ATUALIZA_CONTADOR
 
-; ########################## UNPACK
+; ###################################
+; ##            UNPACK             ##
+; ###################################
+
 UNPACK $ /0001            ; Guarda a posição de retorno
 LD PACKED                      ; Carrega X (PACKED) no acumulador
 / CONST_100               ; Divide X por 100 (desloca para a direita)
