69c6f6198a5b93ee5d6daf1b179b7506e5538378
Adriano Valente <dri9595@gmail.com>
2015-09-24 13:23:39 -0300

    Arrumando sinais de entrada e saída

--- exp3/TYG14A03E01_rotinas.asm
+++ exp3/TYG14A03E01_rotinas.asm
@@ -1,22 +1,36 @@
 ; Importações de constantes
 CONST_100       <
 CONST_01        <
+CONST_02        <
+CONST_FF        <
 
 ; Variáveis para a subrotina PACK
-PACK            <
-PACK_ENTRADA1   >
-PACK_ENTRADA2   >
-PACK_SAIDA      >
+PACK            >
+PACK_ENTRADA1   <
+PACK_ENTRADA2   <
+PACK_SAIDA      <
 
 ; Variáveis para a subrotina UNPACK
-UNPACK          <
-UNPACK_ENTRADA  >
-UNPACK_SAIDA1   >
-UNPACK_SAIDA2   >
+UNPACK          >
+UNPACK_ENTRADA  <
+UNPACK_SAIDA1   <
+UNPACK_SAIDA2   <
+
+; Variáveis para a subrotina STRCOMP
+STRCOMP         >
+LOAD            <
+PRIMEIRA        <
+SEGUNDA         <
+SAIDA_STRCOMP   <
 
 ; Variáveis auxiliares
 AUX             $ /0001
-
+A               $ /0001
+B               $ /0001
+A_1             $ /0001
+A_2             $ /0001
+B_1             $ /0001
+B_2             $ /0001
 
 ; Subrotina PACK
 PACK            $ /0001
